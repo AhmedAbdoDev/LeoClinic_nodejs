602c545efa47add357415e92768d9e512c23bcd6
Ahmed Abd El-Latif <46932157+AhmedAbdoDev@users.noreply.github.com>
2026-08-06 18:15:25 +0300

    refactor users-patients-module

--- src/modules/patients/patient.validation.js
+++ src/modules/patients/patient.validation.js
@@ -2,7 +2,12 @@ import { z } from "zod";
 
 export const updatePatientSchema = z.object({
   body: z.object({
-    address: z.string().optional(),
-    date_of_birth: z.string().datetime().optional(),
+    address: z.string().trim().min(3).optional(),
+    date_of_birth: z
+      .string()
+      .refine((value) => !Number.isNaN(Date.parse(value)), {
+        message: "Invalid date of birth",
+      })
+      .optional(),
   }),
 });
